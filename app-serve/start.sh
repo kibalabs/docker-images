35cea6914577cb5bf9920340e6044b426dc8a8cf
Krishan Patel <krishan711@gmail.com>
2022-05-19 13:47:13 +0100

    .

--- app-serve/start.sh
+++ app-serve/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-envsubst '${SERVICE_NAME} ${VERSION}' < nginx.conf > /etc/nginx/nginx.conf
+envsubst '${NAME} ${VERSION} ${ENVIRONMENT}' < nginx.conf > /etc/nginx/nginx.conf
 
 export KRT_NAME=$SERVICE_NAME
 export KRT_VERSION=$VERSION
